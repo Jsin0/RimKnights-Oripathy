706c6343f929133a435895bd4369bb64937d9b3a
Jsin0 <112178980+Jsin0@users.noreply.github.com>
2025-01-15 00:06:31 -0800

    Initial commit

---
+++ Source/Oripathy/HediffComp_DelayedExplodeOnDeath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Oripathy
+{
+    public class HediffComp_DecayOnDeath : HediffWithComps
+    {
+        public HediffComp_DecayOnDeath Props
+        {
+            get
+            { return (HediffComp_DecayOnDeath)this.Props; }
+        }
+
+    }
+}
---
+++ Source/Oripathy/HediffComp_ExplodeAfterDeath.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using Verse;
+using RimWorld;
+using UnityEngine;
+using Verse.AI;
+using Verse.Sound;
+
+namespace Oripathy
+{
+    public class HediffComp_ExplodeAfterDeath : HediffComp
+    {
+        public HediffCompProperties_ExplodeAfterDeath Props
+        {
+            get
+            { 
+                return (HediffCompProperties_ExplodeAfterDeath)
+                this.Props; 
+            }
+        }
+        /*public override void Notify_PawnKilled()
+        {
+            GenExplosion.DoExplosion(base.Pawn.Position, base.Pawn.Map, this.Props.explosionRadius, this.Props.damageDef, base.Pawn, this.Props.damageAmount, -1f, null, null, null, null, null, 0f, 1, null, false, null, 0f, 1, 0f, false, null, null, null, true, 1f, 0f, true, null, 1f, null, null);
+            if (this.Props.destroyGear)
+            {
+                base.Pawn.equipment.DestroyAllEquipment(DestroyMode.Vanish);
+                base.Pawn.apparel.DestroyAll(DestroyMode.Vanish);
+            }
+        }*/
+
+        public override void Notify_PawnDied(DamageInfo? dinfo, Hediff culprit = null)
+        {
+            base.Notify_PawnDied(dinfo, culprit);
+            this.TryTriggerCountdownShatter();
+        }
+
+        private void TryTriggerCountdownShatter()
+        {
+            if (this.shattering = true)
+            {
+                return;
+            }
+            this.shattering = true;
+            this.countdownTimer.Start(GenTicks.TicksGame, HediffComp_ExplodeAfterDeath.shatterCountdownSeconds.SecondsToTicks(), new Action(this.Shatter));
+        }
+
+        private void Shatter()
+        {
+            GenExplosion.DoExplosion(base.Pawn.Position, base.Pawn.Map, this.Props.explosionRadius, this.Props.damageDef, base.Pawn, this.Props.damageAmount, -1f, null, null, null, null, null, 0f, 1, null, false, null, 0f, 1, 0f, false, null, null, null, true, 1f, 0f, true, null, 1f, null, null);
+            if (this.Props.destroyGear)
+            {
+                base.Pawn.equipment.DestroyAllEquipment(DestroyMode.Vanish);
+                base.Pawn.apparel.DestroyAll(DestroyMode.Vanish);
+            }
+        }
+
+        private TickTimer countdownTimer = new TickTimer();
+
+        private static readonly float
+            shatterCountdownSeconds = 3f;
+
+        private bool shattering;
+    }
+}
