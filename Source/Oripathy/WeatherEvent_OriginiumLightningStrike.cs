278c5984f4313d0710799086ac34da293aa0d20e
Jsin0 <112178980+Jsin0@users.noreply.github.com>
2025-02-22 08:32:38 -0800

    Originium lightning strikes

--- Source/Oripathy/WeatherEvent_OriginiumLightningStrike.cs
+++ Source/Oripathy/WeatherEvent_OriginiumLightningStrike.cs
@@ -12,27 +12,20 @@ using RimWorld;
 namespace Originium
 {
     [StaticConstructorOnStartup]
-    public class WeatherEvent_OriginiumLightningStrike : WeatherEvent_LightningFlash
+    public class WeatherEvent_OriginiumLightningStrike : WeatherEvent_LightningStrike
     {
-        public WeatherEvent_OriginiumLightningStrike(Map map)
-            : base(map)
-        {
-        }
 
-        // Token: 0x06006B9E RID: 27550 RVA: 0x00246628 File Offset: 0x00244828
         public WeatherEvent_OriginiumLightningStrike(Map map, IntVec3 forcedStrikeLoc)
             : base(map)
         {
             this.strikeLoc = forcedStrikeLoc;
         }
 
-        // Token: 0x06006B9F RID: 27551 RVA: 0x00246643 File Offset: 0x00244843
         public override void FireEvent()
         {
             WeatherEvent_OriginiumLightningStrike.DoStrike(this.strikeLoc, this.map, ref this.boltMesh);
         }
 
-        // Token: 0x06006BA0 RID: 27552 RVA: 0x0024665C File Offset: 0x0024485C
         public static void DoStrike(IntVec3 strikeLoc, Map map, ref Mesh boltMesh)
         {
             SoundDefOf.Thunder_OffMap.PlayOneShotOnCamera(map);
@@ -43,7 +36,7 @@ namespace Originium
             boltMesh = LightningBoltMeshPool.RandomBoltMesh;
             if (!strikeLoc.Fogged(map))
             {
-                GenExplosion.DoExplosion(strikeLoc, map, 1.9f, RimWorld.DamageDefOf.Flame, null, -1, -1f, null, null, null, null, Originium.ThingDefOf.RM_OriginiumCluster, 0.1f, 1, null, false, null, 0f, 1, 0f, false, null, null, null, true, 1f, 0f, true, null, 1f, null, null);
+                GenExplosion.DoExplosion(strikeLoc, map, 1.9f, RimWorld.DamageDefOf.Flame, null, -1, -1f, null, null, null, null, Originium.ThingDefOf.RM_OriginiumCluster, 0.15f, 1, null, false, null, 0f, 1, 0f, false, null, null, null, true, 1f, 0f, true, null, 1f, null, null);
                 Vector3 vector = strikeLoc.ToVector3Shifted();
                 for (int i = 0; i < 4; i++)
                 {
@@ -56,19 +49,15 @@ namespace Originium
             SoundDefOf.Thunder_OnMap.PlayOneShot(soundInfo);
         }
 
-        // Token: 0x06006BA1 RID: 27553 RVA: 0x00246792 File Offset: 0x00244992
         public override void WeatherEventDraw()
         {
             Graphics.DrawMesh(this.boltMesh, this.strikeLoc.ToVector3ShiftedWithAltitude(AltitudeLayer.Weather), Quaternion.identity, FadedMaterialPool.FadedVersionOf(WeatherEvent_OriginiumLightningStrike.LightningMat, this.LightningBrightness), 0);
         }
 
-        // Token: 0x04004166 RID: 16742
         private IntVec3 strikeLoc = IntVec3.Invalid;
 
-        // Token: 0x04004167 RID: 16743
         private Mesh boltMesh;
 
-        // Token: 0x04004168 RID: 16744
         private static readonly Material LightningMat = MatLoader.LoadMat("Weather/LightningBolt", -1);
     }
 }
